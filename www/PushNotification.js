3f0f341973648592fd627972e69b4868088d426a
Maxim Kolmogorov <maxim.kolmogorov@gmail.com>
2021-04-15 14:02:47 +0500

    Update plugin.xml
    Added two hooks for plugin.

--- www/PushNotification.js
+++ www/PushNotification.js
@@ -1,4 +1,4 @@
-var exec = require('cordova/exec')
+var exec = require('cordova/exec');
 
 exports.registration = function(success, error) {
   if (error == null) {
@@ -6,14 +6,14 @@ exports.registration = function(success, error) {
   }
 
   if (typeof error != 'function') {
-    console.error('PushNotification.registration failure: error parameter not a function');
+    console.error('pushNotification.registration() failure: error parameter not a function');
     return
   }
 
   if (typeof success != 'function') {
-    console.error('PushNotification.registration failure: success parameter must be a function');
+    console.error('pushNotification.registration() failure: success parameter must be a function');
     return
   }
 
-  exec(success, error, 'PushNotification', 'registration')
+  exec(success, error, 'PushNotification', 'registration');
 };
