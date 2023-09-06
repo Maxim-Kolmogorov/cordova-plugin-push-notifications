906ec0432274675f678d06431ce13c9d376c4c83
Simerl <33984591+SimeonLukas@users.noreply.github.com>
2023-09-06 19:43:29 +0200

    Revert "Update PushNotification.swift"
    This reverts commit 9f3ce97f9c4c50f2fbcb6fa2f59794173e9e0721.

--- src/ios/PushNotification.swift
+++ src/ios/PushNotification.swift
@@ -1,56 +1,136 @@
 import UserNotifications
 
 
-class NotificationService: UNNotificationServiceExtension {
- 
-var contentHandler: ((UNNotificationContent) -> Void)?
-var bestAttemptContent: UNMutableNotificationContent?
- 
-override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-    self.contentHandler = contentHandler
-    bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+// Notification info
+var globalPushNotificationToken: String = ""
+var globalTapedNotification: String = ""
 
-    if let bestAttemptContent = bestAttemptContent {
-        var urlString:String? = nil
-        if let urlImageString = request.content.userInfo["media-attachment-url"] as? String {
-            urlString = urlImageString
-        }
-         
-        if urlString != nil, let fileUrl = URL(string: urlString!) {
-            print("fileUrl: \(fileUrl)")
-             
-            // Download the attachment
-            URLSession.shared.downloadTask(with: fileUrl) { (location, response, error) in
-                if let location = location {
-                    // Move temporary file to remove .tmp extension
-                    if (error == nil) {
-                        let tmpDirectory = NSTemporaryDirectory()
-                        let tmpFile = "file://".appending(tmpDirectory).appending(fileUrl.lastPathComponent)
-                        let tmpUrl = URL(string: tmpFile)!
-                        try! FileManager.default.moveItem(at: location, to: tmpUrl)
-                         
-                        // Add the attachment to the notification content
-                        if let attachment = try? UNNotificationAttachment(identifier: fileUrl.lastPathComponent, url: tmpUrl) {
-                            bestAttemptContent.attachments = [attachment]
-                            }
-                    }
-                    if(error != nil) {
-                        print("Failed to download attachment: \(error.debugDescription)")
-                    }
-                }
-                // Serve the notification content
-                contentHandler(bestAttemptContent)
-            }.resume()
-        }
+// Thread
+let PushNotificationQueue = DispatchQueue(label: "pluginPushNotificationQueue", attributes: .concurrent)
+let PushNotificationSemaphore = DispatchSemaphore(value: 0)
+
+// Extension AppDelegate
+@objc(AppDelegate) extension AppDelegate {
+  // Catch notification if app launched after user touched on message
+  open override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+    if (launchOptions != nil) {
+      if let userInfo = launchOptions?[UIApplication.LaunchOptionsKey.remoteNotification] as? [String : Any] {
+        globalTapedNotification = self.parseAPSObject(obj: userInfo)
+      }
     }
-}
- 
-override func serviceExtensionTimeWillExpire() {
-    // Called just before the extension will be terminated by the system.
-    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-    if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent {
-        contentHandler(bestAttemptContent)
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // Register token
+  open override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    let tokenParts = deviceToken.map { data -> String in
+      return String(format: "%02.2hhx", data)
     }
+    let token = tokenParts.joined()
+    globalPushNotificationToken = token
+    PushNotificationSemaphore.signal()
+  }
+
+  // Token errors handler
+  open override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    print("Failed to register: \(error)")
+    PushNotificationSemaphore.signal()
+  }
+
+  // Catch notification in background (pause mode)
+  open override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+    globalTapedNotification = self.parseAPSObject(obj: userInfo as! [String : Any])
+  }
+    
+  func parseAPSObject(obj: [String : Any]) -> String {
+    let aps = obj["aps"] as? [String : Any]
+    if let str = aps?["payload"] as? String {
+      return str
+    }
+    return ""
+  }
 }
 
+@objc(PushNotification)
+class PushNotification: CDVPlugin {
+
+  private var pushNotificationCallBackID = ""
+
+  @objc(tapped:)
+  private func tapped(command: CDVInvokedUrlCommand) {
+    let res = globalTapedNotification
+    globalTapedNotification = ""
+
+    self.commandDelegate!.send(
+      CDVPluginResult(
+        status: CDVCommandStatus_OK,
+        messageAs: res
+      ),
+      callbackId: command.callbackId
+    )
+  }
+    
+ @objc(registration:)
+ private func registration(command: CDVInvokedUrlCommand) {
+   self.pushNotificationCallBackID = command.callbackId
+   self.registerForPushNotifications()
+ }
+
+  private func registerForPushNotifications() {
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+      (granted, error) in
+      print("UserNotifications permission granted: \(granted)")
+
+      guard granted else {
+        self.pluginError()
+        return
+      }
+      self.getNotificationSettings()
+    }
+  }
+
+  private func getNotificationSettings() {
+    UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+      print("UserNotifications settings: \(settings)")
+      guard settings.authorizationStatus == .authorized else {
+        self.pluginError()
+        return
+      }
+
+      DispatchQueue.main.async {
+        UIApplication.shared.registerForRemoteNotifications()
+      }
+
+      PushNotificationQueue.async {
+        PushNotificationSemaphore.wait(timeout: .distantFuture)
+        if (globalPushNotificationToken.count > 0) {
+          self.pluginReady(token: globalPushNotificationToken)
+        } else {
+          self.pluginError()
+        }
+        PushNotificationSemaphore.signal()
+      }
+    }
+  }
+
+  private func pluginReady(token: String = "") {
+    self.commandDelegate!.send(
+      CDVPluginResult(
+        status: CDVCommandStatus_OK,
+        messageAs: token
+      ),
+      callbackId: self.pushNotificationCallBackID
+    )
+  }
+
+  private func pluginError() {
+    self.commandDelegate!.send(
+      CDVPluginResult(
+        status: CDVCommandStatus_ERROR,
+        messageAs: "Permission denied"
+      ),
+      callbackId: self.pushNotificationCallBackID
+    )
+  }
 }
